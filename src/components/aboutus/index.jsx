4540b3bdb9a7e0c9ba89117a0585d5a0c1df78be
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-04-06 23:58:27 +0530

    refactor: changed src of the images

--- src/components/aboutus/index.jsx
+++ src/components/aboutus/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {ArrowRightIcon} from '@heroicons/react/20/solid'
+import aboutUsImage from '../../assets/aboutus.jpg'
 
 const AboutUs = () => {
   return (
     <div className='bg-white'>
       <div className='flex flex-col lg:flex-row mt-12 items-center py-12'>
-      <div className='lg:w-1/2'><img className='w-3/4 shadow-2xl block mx-auto' src="./public/aboutus.jpg" alt="" /></div>
+      <div className='lg:w-1/2'><img className='w-3/4 shadow-2xl block mx-auto' src={aboutUsImage} alt="" /></div>
       <div className=' lg:w-1/2 font-semibold tracking-wide text-xl mx-8 px-8
       '>
         <h2 className='text-5xl text-textColor font-bold my-8'>Who are we?</h2>
@@ -44,7 +45,7 @@ const AboutUs = () => {
           </li>
         </ul>
       </div>
-      <div className='lg:w-1/2 '><img className='w-3/4 block shadow-2xl mx-auto' src="./public/aboutus.jpg" alt="" /></div>
+      <div className='lg:w-1/2 '><img className='w-3/4 block shadow-2xl mx-auto' src={aboutUsImage} alt="" /></div>
     </div>
     </div>
     
