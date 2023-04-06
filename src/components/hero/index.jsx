3fa9ebd1079cc78c09759f24ed533cbb320c2411
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-04-06 21:52:23 +0530

    style: responsive heor component

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -3,13 +3,13 @@ import heroImg from "../../assets/hero-img.svg";
 import { ArrowRightIcon } from "@heroicons/react/20/solid";
 const Hero = () => {
   return (
-    <div className=" flex text-textColor flex-col-reverse lg:flex-row items-center justify-around w-3/4 mx-auto h-[85vh]">
+    <div className=" flex text-textColor flex-col-reverse lg:flex-row items-center justify-around w-3/4 mx-auto pb-12 h-[85vh]">
       <div className=" py-2 lg:py-8 w-full lg:w-2/3">
-        <div className=" py-8 h-full font-bold text-center lg:text-6xl lg:text-left">
+        <div className=" pt-8 h-full font-bold text-5xl text-center lg:text-7xl lg:text-left">
           Join CoCode and become a part of this exciting community
         </div>
-        <div className="bg-none mx-auto text-base p-[2px] w-fit lg:mx-4">
-          <button className=" transition-all duration-300 w-36 px-3.5 py-1 text-textColor hover:text-white hover:bg-textColor border-solid border-[3px] rounded-lg border-textColor  bg-none">
+        <div className="bg-none  mx-auto text-base w-fit lg:ml-0">
+          <button className=" transition-all duration-300 w-36  py-1 text-textColor hover:text-white hover:bg-textColor border-solid border-[3px] rounded-lg border-textColor  bg-none">
             <span className="text-lg  font-[600] ">Join now</span>{" "}
           </button>
         </div>
